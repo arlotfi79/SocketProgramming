36eef573513d15ecf34de6f88fb2feb36424f2ad
arlotfi79 <lotfia@purdue.edu>
2024-04-02 12:23:02 -0400

    Queue implementation added

---
+++ lab5/lib/queue.h
@@ -0,0 +1,26 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+typedef struct {
+    int *array;
+    int front;
+    int rear;
+    int size;
+    int capacity;
+} Queue;
+
+Queue* createQueue(int capacity);
+void destroyQueue(Queue* queue);
+
+int isEmpty(Queue* queue);
+int isFull(Queue* queue);
+
+void display(Queue* queue);
+
+void enqueue(Queue* queue, int item);
+int dequeue(Queue* queue);
+int front(Queue* queue);
+int rear(Queue* queue);
+
+
+#endif //QUEUE_H
